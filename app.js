2ccb3428bf82573e4ec13e752fc511a52c833910
iv <consmebius@gmail.com>
2020-05-06 23:48:37 +0300

    end

--- app.js
+++ app.js
@@ -6,13 +6,21 @@ const userBlockForm = document.querySelector(".form__user");
 const collapsible = document.querySelector(".collapsible");
 const userForm = document.forms["addUser"];
 
+// Отправляет форму
 userForm.addEventListener("submit", submitHendler);
 
 // Скрывает показывает форму
-addBtn.addEventListener("click", showFormHendler)
+addBtn.addEventListener("click", showFormHendler);
+
 function showFormHendler(event) {
     event.preventDefault();
     userBlockForm.classList.toggle("hide");
+    const i = addBtn.querySelector(".material-icons");
+    if (i.textContent == "add") {
+        i.textContent = "remove";
+    } else if (i.textContent == "remove") {
+        i.textContent = "add";
+    }
 }
 
 // Добавляет нового usera на страницу
@@ -29,14 +37,22 @@ function submitHendler(event) {
 
     collapsible.insertAdjacentElement("afterbegin", liNewUser);
     userForm.reset();
+    postUser(newUser, (user) => {
+        console.log(user);
+    });
     // userBlockForm.classList.toggle("hide");
 
 }
 
 // Формирует userObject
 function createUserObject(name, email, username, phone, website) {
-    const userObject = {name, email, username, phone, website}
-    console.log(userObject);
+    const userObject = {
+        name,
+        email,
+        username,
+        phone,
+        website
+    };
     return userObject;
 }
 
@@ -56,6 +72,24 @@ function getUsers(cb) {
     xhr.send();
 }
 
+// AJAX POST
+function postUser(object, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://jsonplaceholder.typicode.com/users");
+    xhr.addEventListener("load", () => {
+        const user = JSON.parse(xhr.responseText);
+        cb(user);
+    });
+
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8")
+
+    xhr.addEventListener("error", () => {
+        console.log("ERROR");
+    });
+
+    xhr.send(JSON.stringify(object));
+}
+
 // Формирует фрагмент из полученных данных с сервера
 getUsers((users) => {
     const fragment = document.createDocumentFragment();
@@ -63,33 +97,44 @@ getUsers((users) => {
         fragment.appendChild(creaeLi(user));
     })
     collapsible.appendChild(fragment);
-})
+});
 
 // Создает li для одного юзера
 function creaeLi(user) {
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    div.classList.add("collapsible-header");
-    const divBody = document.createElement("div");
-    divBody.classList.add("collapsible-body");
-    const p = document.createElement("p");
-    const i = document.createElement("i");
-    i.classList.add("material-icons");
-    i.textContent ="account_box";
+    const li = accCreateElement("li");
+    const div = accCreateElement("div", ["collapsible-header"]);
+    const divBody = accCreateElement("div", ["collapsible-body"]);
+    const i = accCreateElement("i", ["material-icons"], "account_box");
     div.appendChild(i);
     div.append(user.name);
     divBody.insertAdjacentHTML('afterbegin',
-    `<p>Username: <b>${user.username}</b></p>
+        `<p>Username: <b>${user.username}</b></p>
     <p>E-Mail: <a href="mailto:${user.email}">${user.email}</a></p>
     <p>Phone: <a href="tel:${user.phone}">${user.phone}</a></p>
-    <p>Website: <a href="${user.website}">${user.website}</a></p>`)
+    <p>Website: <a href="${user.website}">${user.website}</a></p>`);
     li.appendChild(div);
     li.appendChild(divBody);
-    return li
+    return li;
+}
+
+// Вспомогательная функция создает елементы, добавляет классы и контент
+function accCreateElement(el, arrClass, cont) {
+    const element = document.createElement(el);
+    if (arrClass) {
+        arrClass.forEach(item => {
+            element.classList.add(item);
+        })
+    }
+    if (cont) {
+        element.append(cont);
+    }
+    return element;
 }
 
 // Materialize code for accordion
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const elem = document.querySelector('.collapsible');
-    const instance = M.Collapsible.init(elem, {accordion: false});
-});
+    const instance = M.Collapsible.init(elem, {
+        accordion: false
+    });
+})
